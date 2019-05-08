f878a91cd8affa60af1b03d1fff3f4878e79c144
Pau-za <molo_pau@hotmail.com>
2019-05-08 13:51:05 -0500

    agrega primera parte del menú, donde aparece el nombre del cliente y el tipo de alimentos. Falta terminar de alinearlo y dejar el espacio para la parte de la cuenta del cliente

---
+++ src/components/GeneralMenu.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="general-menu">
+    <div class="upper-menu">
+      <div class="menu-name">
+        <img
+          id="sm-burger-logo"
+          src="../assets/logo_burquerqueen/logo_burguer_sm.png"
+          alt="logo-burgerqueen"
+        >
+        <h5 id="kind-of-menu">{{ kindOfMenu }}</h5>
+
+        <img id="sm-client-icon" src="../assets/client/client_sm.png" alt>
+        <h4 id="client-name">{{ clientName }}</h4>
+      </div>
+      <div class="menu-number"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GeneralMenu",
+  data() {
+    return {
+      kindOfMenu: "Comida",
+      clientName: "Fulana"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h5,
+h4 {
+  color: rgb(251, 252, 253);
+  display: -webkit-inline-box;
+}
+.general-menu {
+  background-color: #242e3a;
+}
+
+.upper-menu {
+  background-color: #38404a;
+}
+
+/* .menu-name {
+  display:con;
+} */
+
+#sm-burger-logo {
+  margin: 0% 0% -1% -60%;
+}
+
+#kind-of-menu {
+  margin: 0% 3% 1% 9%;
+}
+
+#sm-client-icon {
+  width: 3%;
+  height: auto;
+  margin-right: 1%;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import FoodElection from '@/components/FoodElection';
+import GeneralMenu from '@/components/GeneralMenu'
 import MealsFood from '@/components/MealsFood';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'FoodElection',
+    //   component: FoodElection,
+    // },
     {
       path: '/',
-      name: 'FoodElection',
-      component: FoodElection,
+      name: 'GeneralMenu',
+      component: GeneralMenu,
     },
     {
       path: '/',
