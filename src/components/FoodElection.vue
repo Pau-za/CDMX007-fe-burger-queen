411a647c983e94e9a6709c24195902ff3a2a2195
Pau-za <molo_pau@hotmail.com>
2019-05-08 12:24:58 -0500

    agrega pantalla para elección de tipo de menú. Ahora voy a desarrollar la pantalla que muestra el menú principal de la app

---
+++ src/components/FoodElection.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="food-election">
+    <div class="col l4 m4 s12 offset-m4 food-type" id="breakfast">
+      <img src="../assets/breakfast_copy.png" alt="breakfast-image">
+      <h4>Desayunos</h4>
+    </div>
+    <div class="food-type col l4 m4 s12 offset-m4" id="meal">
+      <img src="../assets/meals_copy.png" alt="meals-image">
+      <h4>Comidas</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FoodElection"
+  //   data() {
+
+  //   }
+};
+</script>
+
+<style scoped>
+h4 {
+  color: rgb(253, 253, 253);
+}
+.food-election {
+  background-color: #242e3a;
+  margin: -6% -1%;
+  padding: 21% 37%;
+  display: inline-flex;
+  align-items: baseline;
+}
+
+#breakfast{
+    margin-right: 89%;
+    margin-left: -48%;
+}
+</style>
+
+
---
+++ src/components/MealsFood.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="meals-food">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MealsFood',
+};
+</script>
+
+<style scoped>
+
+.meals-food{
+    background-color: #242e3a;
+}
+
+</style>
